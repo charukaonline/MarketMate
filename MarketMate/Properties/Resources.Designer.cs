96d6c480471a988aa990cfbed27d78569970d5a8
charukaonline <nuwanthakonline@gmail.com>
2024-12-26 09:36:02 +0530

    manage products, stocks ui changes 1

--- MarketMate/Properties/Resources.Designer.cs
+++ MarketMate/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace MarketMate.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap products {
+            get {
+                object obj = ResourceManager.GetObject("products", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap supermaket {
             get {
                 object obj = ResourceManager.GetObject("supermaket", resourceCulture);
--- MarketMate/adminForms/ManageStock.Designer.cs
+++ MarketMate/adminForms/ManageStock.Designer.cs
@@ -28,22 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Panel6 = new Guna.UI2.WinForms.Guna2Panel();
+            this.manageStocksBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel5 = new Guna.UI2.WinForms.Guna2Panel();
+            this.categoriesBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel4 = new Guna.UI2.WinForms.Guna2Panel();
+            this.mngProductsBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
+            this.addStocksBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2Panel1.SuspendLayout();
+            this.guna2Panel6.SuspendLayout();
+            this.guna2Panel5.SuspendLayout();
+            this.guna2Panel4.SuspendLayout();
+            this.guna2Panel3.SuspendLayout();
+            this.guna2Panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.DarkGreen;
+            this.guna2Panel1.Controls.Add(this.guna2Panel6);
+            this.guna2Panel1.Controls.Add(this.guna2Panel5);
+            this.guna2Panel1.Controls.Add(this.guna2Panel4);
+            this.guna2Panel1.Controls.Add(this.guna2Panel3);
+            this.guna2Panel1.Controls.Add(this.guna2Panel2);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
+            this.guna2Panel1.Size = new System.Drawing.Size(1040, 89);
+            this.guna2Panel1.TabIndex = 2;
+            // 
+            // guna2Panel6
+            // 
+            this.guna2Panel6.Controls.Add(this.manageStocksBtn);
+            this.guna2Panel6.Dock = System.Windows.Forms.DockStyle.Left;
+            this.guna2Panel6.Location = new System.Drawing.Point(827, 0);
+            this.guna2Panel6.Name = "guna2Panel6";
+            this.guna2Panel6.ShadowDecoration.Parent = this.guna2Panel6;
+            this.guna2Panel6.Size = new System.Drawing.Size(195, 89);
+            this.guna2Panel6.TabIndex = 4;
+            // 
+            // manageStocksBtn
+            // 
+            this.manageStocksBtn.CheckedState.Parent = this.manageStocksBtn;
+            this.manageStocksBtn.CustomImages.Parent = this.manageStocksBtn;
+            this.manageStocksBtn.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.manageStocksBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.manageStocksBtn.ForeColor = System.Drawing.Color.SeaGreen;
+            this.manageStocksBtn.HoverState.Parent = this.manageStocksBtn;
+            this.manageStocksBtn.Location = new System.Drawing.Point(17, 22);
+            this.manageStocksBtn.Name = "manageStocksBtn";
+            this.manageStocksBtn.ShadowDecoration.Parent = this.manageStocksBtn;
+            this.manageStocksBtn.Size = new System.Drawing.Size(166, 45);
+            this.manageStocksBtn.TabIndex = 2;
+            this.manageStocksBtn.Text = "Manage Suppliers";
+            // 
+            // guna2Panel5
+            // 
+            this.guna2Panel5.Controls.Add(this.categoriesBtn);
+            this.guna2Panel5.Dock = System.Windows.Forms.DockStyle.Left;
+            this.guna2Panel5.Location = new System.Drawing.Point(617, 0);
+            this.guna2Panel5.Name = "guna2Panel5";
+            this.guna2Panel5.ShadowDecoration.Parent = this.guna2Panel5;
+            this.guna2Panel5.Size = new System.Drawing.Size(210, 89);
+            this.guna2Panel5.TabIndex = 3;
+            // 
+            // categoriesBtn
+            // 
+            this.categoriesBtn.CheckedState.Parent = this.categoriesBtn;
+            this.categoriesBtn.CustomImages.Parent = this.categoriesBtn;
+            this.categoriesBtn.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.categoriesBtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categoriesBtn.ForeColor = System.Drawing.Color.SeaGreen;
+            this.categoriesBtn.HoverState.Parent = this.categoriesBtn;
+            this.categoriesBtn.Location = new System.Drawing.Point(16, 22);
+            this.categoriesBtn.Name = "categoriesBtn";
+            this.categoriesBtn.ShadowDecoration.Parent = this.categoriesBtn;
+            this.categoriesBtn.Size = new System.Drawing.Size(171, 45);
+            this.categoriesBtn.TabIndex = 1;
+            this.categoriesBtn.Text = "Add Suppliers";
+            // 
+            // guna2Panel4
+            // 
+            this.guna2Panel4.Controls.Add(this.mngProductsBtn);
+            this.guna2Panel4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.guna2Panel4.Location = new System.Drawing.Point(409, 0);
+            this.guna2Panel4.Name = "guna2Panel4";
+            this.guna2Panel4.ShadowDecoration.Parent = this.guna2Panel4;
+            this.guna2Panel4.Size = new System.Drawing.Size(208, 89);
+            this.guna2Panel4.TabIndex = 2;
+            // 
+            // mngProductsBtn
+            // 
+            this.mngProductsBtn.CheckedState.Parent = this.mngProductsBtn;
+            this.mngProductsBtn.CustomImages.Parent = this.mngProductsBtn;
+            this.mngProductsBtn.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.mngProductsBtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mngProductsBtn.ForeColor = System.Drawing.Color.SeaGreen;
+            this.mngProductsBtn.HoverState.Parent = this.mngProductsBtn;
+            this.mngProductsBtn.Location = new System.Drawing.Point(17, 22);
+            this.mngProductsBtn.Name = "mngProductsBtn";
+            this.mngProductsBtn.ShadowDecoration.Parent = this.mngProductsBtn;
+            this.mngProductsBtn.Size = new System.Drawing.Size(171, 45);
+            this.mngProductsBtn.TabIndex = 1;
+            this.mngProductsBtn.Text = "Manage Stocks";
+            // 
+            // guna2Panel3
+            // 
+            this.guna2Panel3.Controls.Add(this.addStocksBtn);
+            this.guna2Panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.guna2Panel3.Location = new System.Drawing.Point(199, 0);
+            this.guna2Panel3.Name = "guna2Panel3";
+            this.guna2Panel3.ShadowDecoration.Parent = this.guna2Panel3;
+            this.guna2Panel3.Size = new System.Drawing.Size(210, 89);
+            this.guna2Panel3.TabIndex = 1;
+            // 
+            // addStocksBtn
+            // 
+            this.addStocksBtn.CheckedState.Parent = this.addStocksBtn;
+            this.addStocksBtn.CustomImages.Parent = this.addStocksBtn;
+            this.addStocksBtn.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.addStocksBtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addStocksBtn.ForeColor = System.Drawing.Color.SeaGreen;
+            this.addStocksBtn.HoverState.Parent = this.addStocksBtn;
+            this.addStocksBtn.Location = new System.Drawing.Point(19, 22);
+            this.addStocksBtn.Name = "addStocksBtn";
+            this.addStocksBtn.ShadowDecoration.Parent = this.addStocksBtn;
+            this.addStocksBtn.Size = new System.Drawing.Size(173, 45);
+            this.addStocksBtn.TabIndex = 0;
+            this.addStocksBtn.Text = "Add Stocks";
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.Controls.Add(this.label1);
+            this.guna2Panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.guna2Panel2.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.ShadowDecoration.Parent = this.guna2Panel2;
+            this.guna2Panel2.Size = new System.Drawing.Size(199, 89);
+            this.guna2Panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(22, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Stock Management";
             // 
             // ManageStock
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1040, 594);
+            this.Controls.Add(this.guna2Panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "ManageStock";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ManageStock";
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel6.ResumeLayout(false);
+            this.guna2Panel5.ResumeLayout(false);
+            this.guna2Panel4.ResumeLayout(false);
+            this.guna2Panel3.ResumeLayout(false);
+            this.guna2Panel2.ResumeLayout(false);
+            this.guna2Panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel6;
+        private Guna.UI2.WinForms.Guna2Button manageStocksBtn;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel5;
+        private Guna.UI2.WinForms.Guna2Button categoriesBtn;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel4;
+        private Guna.UI2.WinForms.Guna2Button mngProductsBtn;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel3;
+        private Guna.UI2.WinForms.Guna2Button addStocksBtn;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private System.Windows.Forms.Label label1;
     }
 }
